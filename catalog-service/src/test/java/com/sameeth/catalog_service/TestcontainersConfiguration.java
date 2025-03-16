dcfcdbf38acd8b7e4b86f380e37e4f19ac83f513
sameicp <same.eth2001@gmail.com>
2025-03-16 03:05:59 +0200

    [catalog-service] module set-up

--- catalog-service/src/test/java/com/sameeth/catalog_service/TestcontainersConfiguration.java
+++ catalog-service/src/test/java/com/sameeth/catalog_service/TestcontainersConfiguration.java
@@ -1,6 +1,5 @@
 package com.sameeth.catalog_service;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
@@ -10,10 +9,9 @@ import org.testcontainers.utility.DockerImageName;
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
-	@Bean
-	@ServiceConnection
-	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
-	}
-
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
+    }
 }
